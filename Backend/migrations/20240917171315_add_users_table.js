f0922003087dd7d1806385099c3a7842b0cdae8c
iamswet <isitswet@gmail.com>
2024-09-18 13:19:27 +0530

    a

---
+++ Backend/migrations/20240917171315_add_users_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.createTable('developer',(table)=>{
+        table.increments('id').primary()
+        table.string('name')
+        table.string('email')
+        table.timestamps(true,true)
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.schema.dropTable('developer')
+};
